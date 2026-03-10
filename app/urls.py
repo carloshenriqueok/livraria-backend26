5a34b4a53f4c5229529ba65050d378bb179ee914
carloshenriqueok <ifcarloshenrique@gmail.com>
2026-03-10 16:03:07 -0300

    FEAT: criação da API de livro

--- app/urls.py
+++ app/urls.py
@@ -7,13 +7,14 @@ from drf_spectacular.views import (
 )
 from rest_framework.routers import DefaultRouter
 
-from core.views import AutorViewSets, CategoriaViewSet, EditoraViewSet, UserViewSet 
+from core.views import AutorViewSets, CategoriaViewSet, EditoraViewSet, LivroViewSet,UserViewSet 
 
 router = DefaultRouter()
 
 router.register(r'autores', AutorViewSets, basename='autores')
 router.register(r'categorias', CategoriaViewSet, basename='categorias')
 router.register(r'editoras', EditoraViewSet, basename='editoras')
+router.register(r'livros', LivroViewSet, basename='livros')
 router.register(r'usuarios', UserViewSet, basename='usuarios')
 
 
